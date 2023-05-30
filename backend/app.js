7912d4ce6e88d841657a678150956723e16fc8bc
Jay Patel <jaypatel@Jays-MBP.fios-router.home>
2023-05-30 18:14:10 -0400

    saving day1's work

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,9 @@ var dotenv = require('dotenv').config()
 var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+var topArtistsRouter = require('./routes/topArtists');
+
 const bodyParser = require('body-parser');
 var app = express();
 app.use((req, res, next) => {
@@ -30,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/topArtists', topArtistsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,16 @@ import { Box } from "@mui/system";
 import { Routes, Route } from "react-router-dom";
 import Profile from "./Components/Profile/Profile";
 import NavigationBar from "./Components/Navigation/NavigationBar";
+import TopArtists from "./Components/Discover/topArtists";
 function App() {
   return (
     <div className="App">
       <NavigationBar></NavigationBar>
+      <TopArtists></TopArtists>
       <Box sx={{ marginTop: "100px" }}>
         <Routes>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/home" element={<TopArtists/>} />
         </Routes>
       </Box>
     </div>
