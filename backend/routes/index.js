7e473cdf5e15ea7351ce29ac938055e8136a29fa
RichyKim12 <rmk9ds@virginia.edu>
2023-05-30 14:35:40 -0400

    Added Firebase

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const db = require("./firebase")
+const  {deleteDoc, updateDoc, setDoc, getDocs, collection, doc} = require("firebase/firestore")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/info", async (req, res, next) => {
+  const allDocData = []
+  // console.log(req.query)  // shows the URL params (stuff after the ? in the URL)
+  const docs = await getDocs(collection(db, "User"))
+  docs.forEach((doc) => allDocData.push([doc.data(),doc.id]))
+  docs.forEach((doc) => console.log(doc.data()))
+  res.json({result: allDocData})
+})
 module.exports = router;
