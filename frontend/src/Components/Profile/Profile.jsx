f65596418cfa34ff87557cba2daf5823f001cdbc
EthanHaller <ttk4ey@virginia.edu>
2023-05-30 17:08:06 -0400

    starting to position settings and avatar

--- frontend/src/Components/Profile/Profile.jsx
+++ frontend/src/Components/Profile/Profile.jsx
@@ -1,7 +1,21 @@
+import './profile.css';
+import { Stack, Box, IconButton, Avatar } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+
 function Profile() {
   return (
     <>
-      <h1>Profile</h1>
+      <Stack direction="row" alignItems='flex-end'>
+        <Box sx={{ flexGrow: 1 }} />
+        <IconButton id='settingsButton' aria-label='settings' size='large'>
+          <SettingsIcon fontSize="inherit"/>
+        </IconButton>
+      </Stack>
+      <Box>
+        <Avatar id='userAvatar'>
+
+        </Avatar>
+      </Box>
     </>
   );
 }
