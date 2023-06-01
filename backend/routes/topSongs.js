94f6ae6a30e39cd7b9c2d422cebf57d416e24880
ZaidFada231 <zaidfada01@gmail.com>
2023-06-01 10:42:52 -0400

    setup my top songs api call

--- backend/routes/topSongs.js
+++ backend/routes/topSongs.js
@@ -9,12 +9,13 @@ const { deleteDoc, updateDoc, setDoc, getDocs, collection, doc } = require("fire
 
 router.get('/', async (req, res, next) => {
   try {
+    const timeRange = req.query.time_range || "medium_term"; // Default to medium_term if not provided
     // Make a call to the Spotify API to retrieve the user's top songs
     const response = await axios.get(
-      'https://api.spotify.com/v1/me/top/tracks?limit=1',
+      `https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}&limit=8`,
       {
         headers: {
-          Authorization: `Bearer ${req.AccessToken}`, // Replace with the user's access token
+          Authorization: `Bearer BQCVwpVEcNuZfqKxJT_lzHVZIWaKtq5tGikSPaaEGoymxsaLXMm1KF4IhGPtKf7wpcExF95_dtMaaiNxNeoVAXUCAi_rm4kjD95q_RmVmcMOmY8f1nyRXOPj9mxMdoYiwHAa3UI4FiBAFiyufd8b_qMHQbi430hwQvgCLzBfMK71eNZN5-NtQBNdxqQdsBuVKmL9zlL0x6o`,
         },
       }
     );
