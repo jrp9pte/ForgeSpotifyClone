37f8a04baedcf9f92a046b492a45869e8e7c86a2
ZaidFada231 <zaidfada01@gmail.com>
2023-06-01 13:44:02 -0400

    added popularity modal

--- frontend/src/Components/MyMusic/MyMusic.js
+++ frontend/src/Components/MyMusic/MyMusic.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import MyTopArtists from "./MyTopArtists";
 import MyLikedSongs from "./MyLikedSongs";
 import MyTopTracks from "./MyTopTracks";
-
+import { Dialog, DialogTitle, DialogContent, Button } from "@mui/material";
 const MyMusic = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const toggleDialog = () => {
+    setIsDialogOpen(!isDialogOpen);
+  };
+
   return (
     <div>
       <h1>My Music</h1>
+
       <h2>My Top 8 Artists</h2>
       <MyTopArtists />
+      <div>
+        <br></br>
+        <Button onClick={toggleDialog}>What is Popularity?</Button>
+        <Dialog open={isDialogOpen} onClose={toggleDialog}>
+          <DialogTitle>What is Popularity?</DialogTitle>
+          <DialogContent>
+            <p>
+              The popularity of a track/ artist is a value between 0 and 100,
+              with 100 being the most popular. The popularity is calculated by
+              algorithm and is based, in the most part, on the total number of
+              plays the track has had and how recent those plays are. Generally
+              speaking, songs that are being played a lot now will have a higher
+              popularity than songs that were played a lot in the past.
+              Duplicate tracks (e.g. the same track from a single and an album)
+              are rated independently. Artist and album popularity is derived
+              mathematically from track popularity
+            </p>
+          </DialogContent>
+          <Button onClick={toggleDialog}>Close</Button>
+        </Dialog>
+      </div>
       <h2>My Top 8 Tracks</h2>
       <MyTopTracks />
       <h2>My 8 Most Recent Liked Songs</h2>
