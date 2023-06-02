43c83d128f4b01a53abd59f335dc1e1c4529756d
EthanHaller <ttk4ey@virginia.edu>
2023-06-02 10:37:46 -0400

    some test messages

--- frontend/src/Components/Messages/Conversation.js
+++ frontend/src/Components/Messages/Conversation.js
@@ -1,7 +1,7 @@
 import { Box, Paper, Typography } from "@mui/material"
 import React from "react"
 
-function Conversation() {
+function Conversation({user}) {
 	const testMessages = [
 		{ user: "user1", text: "Hello!" },
 		{ user: "user2", text: "Hey, how are you?" },
@@ -14,23 +14,48 @@ function Conversation() {
 	const displayMessages = testMessages.map((message) => {
 		if (message.user === "user1") {
 			return (
-				<Paper maxWidth="200px" sx={{ justifySelf: "flex-end" }}>
-					<Typography variant="body1">{message.text}</Typography>
-				</Paper>
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "flex-end",
+						mr: "100px",
+					}}
+				>
+					<Paper maxWidth="200px" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', p: "15px" }}>
+						<Typography variant="h6">You:</Typography>
+						<Typography variant="body1">{message.text}</Typography>
+					</Paper>
+				</Box>
 			)
 		} else {
 			return (
-				<Paper maxWidth="200px" sx={{ justifySelf: "flex-start" }}>
-					<Typography variant="body1">{message.text}</Typography>
-				</Paper>
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "flex-start",
+						ml: "100px",
+					}}
+				>
+					<Paper maxWidth="200px" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', p: "15px" }}>
+						<Typography variant="h6">{message.user + ":"}</Typography>
+						<Typography variant="body1">{message.text}</Typography>
+					</Paper>
+				</Box>
 			)
 		}
 	})
 
-    console.log(displayMessages)
+	console.log(displayMessages)
 
 	return (
-		<Box sx={{ display: "flex", height: "400px", overflowY: "scroll" }}>
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				height: "400px",
+				overflowY: "scroll",
+			}}
+		>
 			{displayMessages}
 		</Box>
 	)
