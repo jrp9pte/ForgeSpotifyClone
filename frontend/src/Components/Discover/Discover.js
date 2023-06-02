0b0609902e5eb08127650b0d1a3eb07929d0f0ac
EthanHaller <ttk4ey@virginia.edu>
2023-06-01 21:05:53 -0400

    setting up messages

--- frontend/src/Components/Discover/Discover.js
+++ frontend/src/Components/Discover/Discover.js
@@ -1,17 +1,29 @@
-import React from "react";
-import Users from "./Users";
-import TopArtists from "./topArtists";
-import NewReleases from "./newReleases";
+import React, { useState } from "react"
+import Users from "./Users"
+import TopArtists from "./topArtists"
+import NewReleases from "./newReleases"
+import Discussion from "../Discussion/Discussion"
+import { Box, ToggleButton, ToggleButtonGroup } from "@mui/material"
 const Discover = () => {
-  return (
-    <div>
-      <h1>Discover</h1>
-      <h2>Users</h2>
-      <Users />
-      <h2>New Releases</h2>
-      <NewReleases />
-    </div>
-  );
-};
+	const [showMessages, setShowMessages] = useState(false)
 
-export default Discover;
+	return (
+		<div>
+			<h1>Discover</h1>
+			<h2>Users</h2>
+			{/* <Users /> */}
+			<h2>New Releases</h2>
+			<NewReleases />
+
+			<Box>
+				<ToggleButtonGroup exclusive>
+					<ToggleButton selected={!showMessages} onClick={() => setShowMessages(false)}>Forums</ToggleButton>
+					<ToggleButton selected={showMessages} onClick={() => setShowMessages(true)}>Messages</ToggleButton>
+				</ToggleButtonGroup>
+				{showMessages ? <Discussion /> : <Discussion />}
+			</Box>
+		</div>
+	)
+}
+
+export default Discover
---
+++ frontend/src/Components/Messages/Messages.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Messages() {
+
+    const getMessages = () => {
+        
+    }
+}
+
+export default Messages
