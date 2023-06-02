927c6edf5022888b4df5c76a4cd21a89eef3582d
ZaidFada231 <zaidfada01@gmail.com>
2023-06-02 12:54:04 -0400

    sign up database change

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,62 +1,83 @@
 var express = require("express");
 const querystring = require("querystring");
 var router = express.Router();
-const axios = require('axios')
-const db = require("./firebase")
-require('firebase/auth')
+const axios = require("axios");
+const db = require("./firebase");
+require("firebase/auth");
 
-const  {deleteDoc, updateDoc, setDoc, getDocs,getDoc, collection,where, query, doc} = require("firebase/firestore")
-const { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} = require("firebase/auth");
+const {
+  deleteDoc,
+  updateDoc,
+  setDoc,
+  getDocs,
+  getDoc,
+  collection,
+  where,
+  query,
+  doc,
+} = require("firebase/firestore");
+const {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} = require("firebase/auth");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.post("/GetUsername", async(req,res)=>{
-  const {access_token} = req.body
+router.post("/GetUsername", async (req, res) => {
+  const { access_token } = req.body;
   const response = await axios.get("https://api.spotify.com/v1/me", {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
 
-    // Extract the user profile from the API response
+  // Extract the user profile from the API response
   const profile = response.data;
   res.json({ profile });
-})
+});
 
-router.post('/SetUsername', async(req,res)=>{
-  const {username, email, password} = req.body
+router.post("/SetUsername", async (req, res) => {
+  const { username, email, password } = req.body;
   const userCollection = collection(db, "User");
   const q = query(userCollection, where("email", "==", email));
-  const querySnapshot = await getDocs(q)
-  let docid = []
+  const querySnapshot = await getDocs(q);
+  let docid = [];
   querySnapshot.forEach((doc) => {
     docid.push(doc.id);
   });
   const docref = doc(db, "User", docid[0]);
   const update = {
-    username: username
-  }
-  updateDoc(docref,update)
-})
+    username: username,
+  };
+  updateDoc(docref, update);
+});
 
-router.post('/login', async(req,res)=>{
-  const {password, email} = req.body
+router.post("/login", async (req, res) => {
+  const { password, email } = req.body;
 
-  try{
-    const auth = getAuth()
-    const userCredential = await signInWithEmailAndPassword(auth,email,password)
+  try {
+    const auth = getAuth();
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
     // TODO: Need to send error messages back stating invalid email, account already exists,
     // invalid credentials
-    console.log(userCredential)
-    const q = query(collection(db,'User'), where('uid', "==", userCredential.user.uid))
-    const querySnapshot = await getDocs(q)
-    const client_id = process.env.REACT_APP_Client_id
-    const client_secret = process.env.REACT_APP_Client_secret
-    const redirect_uri= "http://localhost:3000/accountcreation"
-    let data = []
-    let docid = []
+    console.log(userCredential);
+    const q = query(
+      collection(db, "User"),
+      where("uid", "==", userCredential.user.uid)
+    );
+    const querySnapshot = await getDocs(q);
+    const client_id = process.env.REACT_APP_Client_id;
+    const client_secret = process.env.REACT_APP_Client_secret;
+    const redirect_uri = "http://localhost:3000/accountcreation";
+    let data = [];
+    let docid = [];
     querySnapshot.forEach((doc) => {
       data.push(doc.data().refresh_token);
       docid.push(doc.id);
@@ -83,7 +104,6 @@ router.post('/login', async(req,res)=>{
         .catch((err) => console.log(err))
         .then((res) => res.json())
         .then((data) => {
-
           const result = {
             uid: userCredential.user.uid,
             access_token: data.access_token,
@@ -91,15 +111,14 @@ router.post('/login', async(req,res)=>{
           };
           const docref = doc(db, "User", docid[0]);
           const update = {
-            access_token: data.access_token
-          }
-          updateDoc(docref,update)
-          
-          res.send(result)
-          
-          // Using new access token, call to spotify and grab username to save to doc
+            access_token: data.access_token,
+          };
+          updateDoc(docref, update);
 
-        })
+          res.send(result);
+
+          // Using new access token, call to spotify and grab username to save to doc
+        });
     } catch (err) {
       console.log(err);
       res.status(500).send(err);
@@ -131,10 +150,11 @@ router.post("/savetodb", async (req, res) => {
       );
       await setDoc(doc(db, "User", Math.random().toString()), {
         email: email,
-        username: "placeholder",
+        username: email.split("@")[0],
         access_token: access_token,
         refresh_token: refresh_token,
         uid: userCredential.user.uid,
+        public: true,
       });
 
       res.send("created!");
@@ -179,13 +199,21 @@ router.post("/spotifycodes", async (req, res) => {
   }
 });
 
-router.get('/spotifyAuthorize', (req,res) =>{
-    const client_id = process.env.REACT_APP_Client_id
-    const scope = "user-top-read user-read-private user-read-email user-library-read";
-    const redirect_uri= "http://localhost:3000/accountcreation"
-    const url = "https://accounts.spotify.com/en/authorize?client_id="+client_id+"&redirect_uri="+redirect_uri+"&scope="+scope+"&response_type=code&show_dialog=true"
-    res.redirect(url)
-  })
+router.get("/spotifyAuthorize", (req, res) => {
+  const client_id = process.env.REACT_APP_Client_id;
+  const scope =
+    "user-top-read user-read-private user-read-email user-library-read";
+  const redirect_uri = "http://localhost:3000/accountcreation";
+  const url =
+    "https://accounts.spotify.com/en/authorize?client_id=" +
+    client_id +
+    "&redirect_uri=" +
+    redirect_uri +
+    "&scope=" +
+    scope +
+    "&response_type=code&show_dialog=true";
+  res.redirect(url);
+});
 
 router.get("/info", async (req, res, next) => {
   const allDocData = [];
@@ -196,52 +224,52 @@ router.get("/info", async (req, res, next) => {
   res.json({ result: allDocData });
 });
 
+router.post("/sendMessage", async (req, res) => {
+  const { refstring, message, currentUsername } = req.body;
 
-router.post("/sendMessage", async(req,res)=>{
-  const {refstring, message, currentUsername} = req.body
-
-  const docref = doc(db,"Messages", refstring)
-  const MessageDoc =  await getDoc(docref)
+  const docref = doc(db, "Messages", refstring);
+  const MessageDoc = await getDoc(docref);
   if (MessageDoc.exists()) {
-    let chats = MessageDoc.data().chats
-    chats.push({[currentUsername]: message})
-    console.log(chats)
-    setDoc(docref,{
+    let chats = MessageDoc.data().chats;
+    chats.push({ [currentUsername]: message });
+    console.log(chats);
+    setDoc(docref, {
       chats: chats,
-    })
+    });
+  } else {
+    console.log("DNE");
   }
-  else{
-    console.log("DNE")
-  }
-}) 
+});
 
-router.post("/getMessageReference", async(req,res)=>{
-    const {uid, access_token, username} = req.body;
-    const userCollection = collection(db, "User");
-    console.log(uid)
-    const q = query(userCollection, where("uid", "==", uid));
-    const querySnapshot = await getDocs(q);
-    let data = [];
-    querySnapshot.forEach((doc) => {
-      data.push(doc.data());
-    })
-    const currentUsername = data[0].username
-    const messageArray = data[0].messages
-    // for(let i =0; i < messageArray.length; ++i){
-    //   console.log(messageArray[i].field)
-    // }
-    
-    messageArray.forEach(function(item){
-      for (var key in item){
-        if (key === username){
-          const specificMessageRef = item[key]
-          res.send({refstring: specificMessageRef.id, currentUsername: currentUsername})
-          console.log(specificMessageRef.id)
-          break;
-        }
+router.post("/getMessageReference", async (req, res) => {
+  const { uid, access_token, username } = req.body;
+  const userCollection = collection(db, "User");
+  console.log(uid);
+  const q = query(userCollection, where("uid", "==", uid));
+  const querySnapshot = await getDocs(q);
+  let data = [];
+  querySnapshot.forEach((doc) => {
+    data.push(doc.data());
+  });
+  const currentUsername = data[0].username;
+  const messageArray = data[0].messages;
+  // for(let i =0; i < messageArray.length; ++i){
+  //   console.log(messageArray[i].field)
+  // }
+
+  messageArray.forEach(function (item) {
+    for (var key in item) {
+      if (key === username) {
+        const specificMessageRef = item[key];
+        res.send({
+          refstring: specificMessageRef.id,
+          currentUsername: currentUsername,
+        });
+        console.log(specificMessageRef.id);
+        break;
       }
-    })
-
-})
+    }
+  });
+});
 
 module.exports = router;
